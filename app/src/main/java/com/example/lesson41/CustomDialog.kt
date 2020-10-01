868da2f507df7bea7938bd476424d7f848dbece3
adilbaev-ruslan <adilbaev-ruslan@umail.uz>
2020-10-01 17:06:00 +0500

    update

--- app/src/main/java/com/example/lesson41/CustomDialog.kt
+++ app/src/main/java/com/example/lesson41/CustomDialog.kt
@@ -1,13 +1,10 @@
 package com.example.lesson41
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.custom_dialog.*
-import java.util.*
-import kotlin.concurrent.schedule
 
 class CustomDialog(context: Context, activity: MainActivity): Dialog(context) {
 
@@ -15,23 +12,12 @@ class CustomDialog(context: Context, activity: MainActivity): Dialog(context) {
 
     var models: MutableList<Homes> = mutableListOf()
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.custom_dialog)
         setData()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(context, 5, GridLayoutManager.VERTICAL, false)
-
-        swipeRefreshLayout.setOnRefreshListener {
-            updateData()
-        }
-
-        swipeRefreshLayout.setColorSchemeColors(
-            R.color.colorAccent,
-            R.color.colorPrimary,
-            R.color.colorPrimaryDark,
-        )
 
         btnPositiv.setOnClickListener {
             adapter.selectedItem()
@@ -42,16 +28,7 @@ class CustomDialog(context: Context, activity: MainActivity): Dialog(context) {
         }
     }
 
-    private fun updateData() {
-        Timer().schedule(2000){
-            adapter.setData(models.shuffled() as MutableList<Homes>)
-            if (swipeRefreshLayout.isRefreshing) {
-                swipeRefreshLayout.isRefreshing = false
-            }
-        }
-    }
-
-    fun setData() {
+    private fun setData() {
         for (i in 0 until 100) {
             var model: Homes = Homes()
             model.item = i + 1
